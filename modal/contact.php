461bedd88769be130e923d7e92b6befc78bd9fe3
fabiohsan <faabioo201415@gmail.com>
2024-09-12 10:32:59 -0300

    Alterações na seção de contato e no modal de envio de email
    - Atualizado o email de contato para "contato@fabiosan.com.br".
    - Traduzido o conteúdo do modal de envio de email (contact.php) para português.
    - Mantida a estrutura original do código, com validações de email e mensagens de confirmação.
    - Links de contato atualizados na página HTML (WhatsApp, Email, LinkedIn, Facebook, etc.) com abertura correta em novas abas.

--- modal/contact.php
+++ modal/contact.php
@@ -1,51 +1,51 @@
 <?php
 
-// Put contacting email here
+// Coloque o email de contato aqui
 $php_main_email = "example@gmail.com";
 
-//Fetching Values from URL
+// Recebendo valores do formulário
 $php_name = $_POST['ajax_name'];
 $php_email = $_POST['ajax_email'];
 $php_message = $_POST['ajax_message'];
 
 
 
-//Sanitizing email
+// Sanitizando o email
 $php_email = filter_var($php_email, FILTER_SANITIZE_EMAIL);
 
 
-//After sanitization Validation is performed
+// Após a sanitização, é realizada a validação
 if (filter_var($php_email, FILTER_VALIDATE_EMAIL)) {
 	
 	
-		$php_subject = "Message from contact form";
+		$php_subject = "Mensagem do formulário de contato";
 		
-		// To send HTML mail, the Content-type header must be set
+		// Para enviar e-mails em HTML, o cabeçalho Content-type deve ser configurado
 		$php_headers = 'MIME-Version: 1.0' . "\r\n";
 		$php_headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-		$php_headers .= 'From:' . $php_email. "\r\n"; // Sender's Email
-		$php_headers .= 'Cc:' . $php_email. "\r\n"; // Carbon copy to Sender
+		$php_headers .= 'De:' . $php_email. "\r\n"; // Email do remetente
+		$php_headers .= 'Cc:' . $php_email. "\r\n"; // Cópia para o remetente
 		
-		$php_template = '<div style="padding:50px;">Hello ' . $php_name . ',<br/>'
-		. 'Thank you for contacting us.<br/><br/>'
-		. '<strong style="color:#f00a77;">Name:</strong>  ' . $php_name . '<br/>'
+		$php_template = '<div style="padding:50px;">Olá ' . $php_name . ',<br/>'
+		. 'Obrigado por entrar em contato conosco.<br/><br/>'
+		. '<strong style="color:#f00a77;">Nome:</strong>  ' . $php_name . '<br/>'
 		. '<strong style="color:#f00a77;">Email:</strong>  ' . $php_email . '<br/>'
-		. '<strong style="color:#f00a77;">Message:</strong>  ' . $php_message . '<br/><br/>'
-		. 'This is a Contact Confirmation mail.'
+		. '<strong style="color:#f00a77;">Mensagem:</strong>  ' . $php_message . '<br/><br/>'
+		. 'Este é um email de confirmação de contato.'
 		. '<br/>'
-		. 'We will contact you as soon as possible .</div>';
+		. 'Entraremos em contato o mais breve possível.</div>';
 		$php_sendmessage = "<div style=\"background-color:#f5f5f5; color:#333;\">" . $php_template . "</div>";
 		
-		// message lines should not exceed 70 characters (PHP rule), so wrap it
+		// As linhas da mensagem não devem exceder 70 caracteres (regra do PHP), então será quebrada
 		$php_sendmessage = wordwrap($php_sendmessage, 70);
 		
-		// Send mail by PHP Mail Function
+		// Enviar email usando a função mail do PHP
 		mail($php_main_email, $php_subject, $php_sendmessage, $php_headers);
 		echo "";
 	
 	
 } else {
-	echo "<span class='contact_error'>* Invalid email *</span>";
+	echo "<span class='contact_error'>* Email inválido *</span>";
 }
 
 ?>
